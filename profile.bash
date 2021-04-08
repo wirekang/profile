46f077d99d49ac36b298bfbfa51400222d2ed830
wirekang <wirekang@gmail.com>
2021-04-09 00:50:48 +0900

    Init

---
+++ profile.bash
@@ -0,0 +1,15 @@
+export NVIM="~/.config/nvim"
+export PATH=$PATH:/usr/local/go/bin
+export PATH="$PATH:~/.local/bin"
+export GOPATH=$(go env GOPATH)
+export GOROOT=$(go env GOROOT)
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+export PATH="$PATH:$(yarn global bin)"
+alias ports='sudo netstat -antp | grep LISTEN'
+alias gits="git status"
+alias gita="git add"
+alias gitc="git commit"
+alias gitp="git push"
+alias gitb="git branch"
+alias gitd="git diff"
