7188a36bd270729689fa0518dd187960906d71dd
wirekang <wirekang@gmail.com>
2021-04-12 08:55:47 +0900

    Add git log alias

--- profile.bash
+++ profile.bash
@@ -13,6 +13,7 @@ alias gitc="git commit"
 alias gitp="git push"
 alias gitb="git branch"
 alias gitd="git diff"
+alias gitl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 export PS1="╔═══ \u \e[1;32m\w\e[0m \$(__git_ps1 '(%s)')\n╚═ "
 PROMPT_COMMAND="export PROMPT_COMMAND=echo"
